44ea973a3f4c37c3d30efbe93987c632d8284d9e
Jack Hu <Jack.Hu@amd.com>
2026-04-10 16:02:54 +0000

    Fixed linting, formatting, etc
    Signed-off-by: Jack Hu <Jack.Hu@amd.com>

--- vllm/v1/attention/backends/rocm_aiter_fa.py
+++ vllm/v1/attention/backends/rocm_aiter_fa.py
@@ -1400,7 +1400,8 @@ class AiterFlashAttentionImpl(AttentionImpl):
     def set_fused_kv_cache_layout(self):
         pass
 
-    def do_qk_norm_rope_kvcache_update(self,
+    def do_qk_norm_rope_kvcache_update(
+        self,
         layer: AttentionLayer,
         qkv: torch.Tensor,
         q_out: torch.Tensor,
@@ -1421,10 +1422,10 @@ class AiterFlashAttentionImpl(AttentionImpl):
             key_cache = key_cache.view(current_platform.fp8_dtype())
             value_cache = value_cache.view(current_platform.fp8_dtype())
 
-        num_heads_q = layer.num_heads
-        num_heads_k = layer.num_kv_heads
-        num_heads_v = layer.num_kv_heads
-        head_dim = layer.head_size
+        num_heads_q = self.num_heads
+        num_heads_k = self.num_kv_heads
+        num_heads_v = self.num_kv_heads
+        head_dim = self.head_size
         use_shuffle_layout = rocm_aiter_ops.is_shuffle_kv_cache_enabled()
         x = 16 // key_cache.element_size()
         block_size = key_cache.shape[1]
@@ -1435,19 +1436,15 @@ class AiterFlashAttentionImpl(AttentionImpl):
         v_scale_val = layer._v_scale_float
         if self._cached_k_scale_val is None or (
             self._cached_k_scale_val != k_scale_val
-            and not (math.isnan(self._cached_k_scale_val)
-                     and math.isnan(k_scale_val))
+            and not (math.isnan(self._cached_k_scale_val) and math.isnan(k_scale_val))
         ):
-            self._cached_k_scale_cpu = torch.tensor(k_scale_val,
-                                                    dtype=torch.float32)
+            self._cached_k_scale_cpu = torch.tensor(k_scale_val, dtype=torch.float32)
             self._cached_k_scale_val = k_scale_val
         if self._cached_v_scale_val is None or (
             self._cached_v_scale_val != v_scale_val
-            and not (math.isnan(self._cached_v_scale_val)
-                     and math.isnan(v_scale_val))
+            and not (math.isnan(self._cached_v_scale_val) and math.isnan(v_scale_val))
         ):
-            self._cached_v_scale_cpu = torch.tensor(v_scale_val,
-                                                    dtype=torch.float32)
+            self._cached_v_scale_cpu = torch.tensor(v_scale_val, dtype=torch.float32)
             self._cached_v_scale_val = v_scale_val
 
         rocm_aiter_ops.hip_qk_norm_rope_and_cache(
